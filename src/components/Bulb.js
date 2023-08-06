2c51af68f71b864c8748176d06eb8f122ae5b224
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-08-05 22:36:32 -0500

    Services Layout

--- src/components/Bulb.js
+++ src/components/Bulb.js
@@ -1,5 +1,17 @@
+import Image from 'next/image';
+
 const Bulb = () => {
-  return <div>Bulb</div>;
+  return (
+    <div>
+      <Image
+        src="/bulb.png"
+        width={260}
+        height={200}
+        alt=''
+        className='absolute -bottom-12 -left-26 z-10 w-[200px] rotate-12 animate-pulse mix-blend-color-dodge duration-75 xl:w-[260px]'
+      />
+    </div>
+  );
 };
 
 export default Bulb;
