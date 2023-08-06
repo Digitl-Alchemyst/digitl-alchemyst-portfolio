eade5e83f02524e4fd723f0bbd2427d5b3f55dc1
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-08-06 00:19:09 -0500

    Testimonial layout

--- src/components/TestimonialSlider.js
+++ src/components/TestimonialSlider.js
@@ -1,3 +1,11 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import Image from 'next/image';
+import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
+
 // testimonial data
 const testimonialData = [
   {
@@ -24,7 +32,57 @@ const testimonialData = [
 ];
 
 const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+  return (
+    <Swiper
+      spaceBetween={10}
+      navigation={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Navigation, Pagination]}
+      className='h-[400px]'
+    >
+      {testimonialData.map((name, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className='flex h-full flex-col items-center gap-x-8 md:flex-row'>
+              {/* Reviewer Info */}
+              <div className='relative mx-auto flex w-full max-w-[300px] flex-col items-center xl:mx-0 xl:justify-center'>
+                <div className='flex flex-col justify-center text-center'>
+                  {/* Avatar */}
+                  <div className='mx-autp mb-2'>
+                    <Image src={name.image} alt='' width={100} height={100} />
+                  </div>
+                  {/* Name */}
+                  <div className='text-lg'>{name.name}</div>
+                  {/* Position  */}
+                  <div className='text-[12px] font-light capitalize tracking-widest'>
+                    {name.position}
+                  </div>
+                </div>
+              </div>
+              {/* Quote */}
+
+              <div className='relative flex flex-1 flex-col justify-center before:w-[1px] xl:pl-20  xl:before:absolute xl:before:left-0 xl:before:h-[200px] xl:before:bg-white/20'>
+                {/* Quotes "" */}
+                <div className='mb-4'>
+                  <FaQuoteLeft className='text-4-xl mx-auto text-white/20 md:mx-0 xl:text-6xl' />
+                </div>
+                {/* Message  */}
+                <div className='text-center md:text-left xl:text-lg'>
+                  {name.message}
+                </div>
+                {/* Quotes "" */}
+                <div className='mt-4 flex justify-end'>
+                  <FaQuoteRight className='text-4-xl mx-auto text-white/20 md:mx-0 xl:text-6xl' />
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default TestimonialSlider;
--- src/pages/testimonials/index.js
+++ src/pages/testimonials/index.js
@@ -1,5 +1,32 @@
+import TestimonialSlider from '@/c/TestimonialSlider';
+import { motion } from 'framer-motion';
+import { fadeIn } from '#/variants';
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className='h-full bg-primary/30 py-32 text-center'>
+      <div className='container mx-auto flex h-full flex-col justify-center'>
+        <motion.h2
+          variants={fadeIn('up', 0.2)}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className='h2 mb-8 xl:mb-0'
+        >
+          What <span className='text-accent'>clients</span> are{' '}
+          <span className='text-accent2'>saying.</span>
+        </motion.h2>
+        <motion.div
+          variants={fadeIn('right', 0.4)}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+        >
+          <TestimonialSlider />
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
